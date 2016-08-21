dadfb1ef31f81f3674d6a4e3daa32531ef7c5d75
Weijia Tang <weijia.tang.18@dartmouth.edu>
2016-08-21 16:40:55 -0400

    author name

--- app/controllers/post_controller.js
+++ app/controllers/post_controller.js
@@ -4,7 +4,7 @@ import Post from '../models/post_model';
 // and we purposefully don't return content here either
 const cleanPosts = (posts) => {
   return posts.map(post => {
-    return { id: post._id, title: post.title, tags: post.tags.toString(), anonymous: post.anonymous, lost: post.lost, authorId: post.authorId };
+    return { id: post._id, title: post.title, tags: post.tags.toString(), anonymous: post.anonymous, lost: post.lost, authorId: post.authorId, authorName: post.authorName };
   });
 };
 
