b14e9774576ba9dbdb2fa09bdb9ac7898000de7b
Weijia Tang <weijia.tang.18@dartmouth.edu>
2016-08-22 15:25:21 -0400

    debug

--- app/controllers/post_controller.js
+++ app/controllers/post_controller.js
@@ -86,7 +86,7 @@ export const updatePost = (req, res) => {
 
   Post.findById(req.params.id)
   .then(post => {
-    res.json({ id: post._id, title: post.title, tags: post.tags.toString(), content: post.content, author: post.authorName, authorId: post.authorId });
+    res.json({ id: post._id, title: post.title, tags: post.tags.toString(), content: post.content, author: post.authorName, authorId: post.authorId, lost: post.lost });
   })
   .catch(error => {
     res.json({ error });
